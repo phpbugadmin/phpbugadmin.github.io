bfa066aa517fe26d7f423d38f6494aeb681f01ee
test <example@github.com>
2024-02-20 09:36:02 +0000

    deploy: phpbugadmin/teachergo-front@69ef5d22ce5cead4289884f1d11582ab621e2073

--- static/js/8881.175d12b1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunkframe"]=self["webpackChunkframe"]||[]).push([[8881],{8881:function(e,n,u){u.r(n),u.d(n,{default:function(){return s}});var a=function(){var e=this,n=e._self._c;return n("div",[e._v(" aaaa ")])},t=[],f={name:"StudentInfo"},r=f,c=u(1001),l=(0,c.Z)(r,a,t,!1,null,"0b3cef76",null),s=l.exports}}]);
